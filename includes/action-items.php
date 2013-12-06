439f40eb6adb8bb4dc784e7138ad2bd1f60c1ae6
somecallmejosh <josh@thebrileys.com>
2013-12-06 10:28:07 -0500

    Update HUDL link

--- includes/action-items.php
+++ includes/action-items.php
@@ -1,5 +1,5 @@
 <div class="action-items">
   <a href="videos.php#call-out" class="button">Games</a>
   <a href="videos.php#field-goals" class="button">Off Season</a>
-  <a href="videos.php#workouts" class="button">Workouts</a>
+  <a href="http://www.hudl.com/athlete/o/1428126/" target="_blank" class="button">HUDL</a>
 </div>
