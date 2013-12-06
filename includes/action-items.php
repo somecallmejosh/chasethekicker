1e65543dae46d65f62c2c66f0de4bc9b9ba6b60d
somecallmejosh <josh@thebrileys.com>
2013-12-06 10:32:15 -0500

    Reorganize aciton links.

--- includes/action-items.php
+++ includes/action-items.php
@@ -1,5 +1,5 @@
 <div class="action-items">
   <a href="videos.php#call-out" class="button">Highlights</a>
   <a href="http://www.hudl.com/athlete/o/1428126/" target="_blank" class="button">HUDL</a>
-  <a href="videos.php#field-goals" class="button">Training</a>
+  <a href="videos.php#kickoffs" class="button">Training</a>
 </div>
