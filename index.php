b5e66398a04c334b5192ac915777dda49e8368bf
somecallmejosh <josh@thebrileys.com>
2014-05-13 21:52:16 -0400

    Change content on home page.

--- index.php
+++ index.php
@@ -3,13 +3,13 @@
     <div class="primary-content">
       <h2 class="call-out">UCONN</h2>
       <img class="pull-right full-border" src="images/chase-briley-field-goal.jpg" alt="Chase Briley Field Goal">
-      <p>My name is <strong>Chase Briley</strong>. I haven't always been a kicker. I spent most of my time growing up 
-      as a soccer player. Clearing balls on defense and taking corner kicks helped prepare me for another adventure. When I entered high school, soccer was no 
-      longer available. I was given the chance to try out for kicker on the football team. It was a blessing in disguise. I love this game. 
+      <p>My name is <strong>Chase Briley</strong> and I am proud to announce that I will be playing for the 
+        University of Connecticut in the Fall of 2014. I will be updating this site with new information as it 
+        becomes available. Thanks for everyone who's helped me in this journey!  
       Here are some of my <a href="videos.php">kicking videos</a>.</p>
     </div>
     <div class="secondary-content">
-      <h3>Highschool Highlights <em>Playlist</em></h3>
+      <h3>Highschool <em>Highlights</em></h3>
       <iframe width="390" height="219" src="//www.youtube.com/embed/videoseries?list=PLidCjvFoClTe62vRmT2ts2T-h2mWUDkkH" frameborder="0" allowfullscreen></iframe>
       <?php include('includes/action-items.php') ?>
     </div>
